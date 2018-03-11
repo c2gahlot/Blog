38156d296e023dbade3de72e207bd2fb0746d5da
Shubham Gahlot <c2gahlot@gmail.com>
2018-03-11 11:30:52 +0530

    Minor improvements added

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-# from data import Artcles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 
-
 app = Flask(__name__)
-
-# Articles = Artcles()
 
 # Config MySQL Database
 app.config['MYSQL_HOST'] = 'localhost'
