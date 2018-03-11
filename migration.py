3b6df3e61170212dd7dddffa866d20dca131093d
Shubham Gahlot <c2gahlot@gmail.com>
2018-03-11 16:55:24 +0530

    Added Migration And Requirements File

---
+++ migration.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/Blog'
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+class Users(db.Model):
+    ___tablename__ = 'users'
+    id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    username = db.Column(db.String(30))
+    password = db.Column(db.String(100))
+    register_date = db.Column(db.TIMESTAMP, nullable=False, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+class Articles(db.Model):
+    ___tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    author = db.Column(db.String(100))
+    body = db.Column(db.Text)
+    create_date = db.Column(db.TIMESTAMP, nullable=False, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+if __name__ == '__main__':
+    manager.run()
